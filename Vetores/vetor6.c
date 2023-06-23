4174cb140abc87a271a43c9322480f7ac159215e
TestDeveloped <137568401+TestDeveloped@users.noreply.github.com>
2023-06-23 19:23:36 -0300

    Create vetor6.c

---
+++ Vetores/vetor6.c
@@ -0,0 +1,6 @@
+/* Elabore programa que solicita e armazena oito números reais em uma matriz 4 × 2, 
+denominada de A. Construa outra matriz, denominada de B, do mesmo tipo e 
+tamanho de A, e com os mesmos elementos armazenados em A, porém de forma 
+invertida. Ou seja, o primeiro elemento de A passa a ser o último de B, o segundo 
+elemento de A passa a ser o penúltimo de B e assim por diante. Ao final, o 
+programa deve exibir o conteúdo de A e B */
