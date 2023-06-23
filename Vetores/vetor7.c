68c683dbd251c596b56dc5d0db56f98318445221
TestDeveloped <137568401+TestDeveloped@users.noreply.github.com>
2023-06-23 19:23:58 -0300

    Create vetor7.c

---
+++ Vetores/vetor7.c
@@ -0,0 +1,15 @@
+/* Crie um programa que solicita e armazena n × m números inteiros em uma matriz n 
+× m, denominada de A, onde n e m também devem ser fornecidos pelo usuário. 
+Construa outra matriz, denominada de B, do mesmo tipo e tamanho de A, onde cada 
+elemento de B deve ser definido pela multiplicação do elemento correspondente de 
+Disciplina: Algoritmos Semestre: 2021.2 Data: 23/05/2022
+A por 3, ou seja, B[i][j] deve ser igual a A[i][j] * 3, onde i e j correspondem aos 
+índices das matrizes. Dessa forma, se A =
+[3 2 1
+4 0 5
+8 6 9]
+, teremos B =
+[9 6 3
+12 0 15
+24 18 27]. 
+Ao final, o programa deve exibir o conteúdo de A e B */
