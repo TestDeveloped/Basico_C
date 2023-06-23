428e4a2580cedd1ab12de3a62c3eb38a94d9d1e7
TestDeveloped <137568401+TestDeveloped@users.noreply.github.com>
2023-06-23 19:23:01 -0300

    Create vetor4.c

---
+++ Vetores/vetor4.c
@@ -0,0 +1,3 @@
+/*  Escreva um programa que solicita e armazena seis números inteiros em uma matriz 
+3 x 2, denominada de A, e solicita e armazena seis números inteiros em outra matriz 
+2 x 3, denominada de B. Em seguida, exiba o produto dessas matrizes. */
