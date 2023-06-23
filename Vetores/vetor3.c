55f00da2296a4349018df5dc44c927d87d033964
TestDeveloped <137568401+TestDeveloped@users.noreply.github.com>
2023-06-23 19:21:38 -0300

    Create vetor3.c

---
+++ Vetores/vetor3.c
@@ -0,0 +1,3 @@
+/*  Faça um programa que solicita e armazena seis números inteiros em uma matriz 2 x 
+3, denominada de A, e solicita e armazena seis números inteiros em outra matriz 2 x 
+3, denominada de B. Em seguida, exiba a soma dessas matrizes */
