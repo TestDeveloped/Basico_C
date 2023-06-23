8c1e9d818ec652b76fc5c692e9002699dbacb9fd
TestDeveloped <137568401+TestDeveloped@users.noreply.github.com>
2023-06-23 19:23:19 -0300

    Create vetor5.c

---
+++ Vetores/vetor5.c
@@ -0,0 +1,6 @@
+/* Crie um programa que solicita e armazena nove temperaturas em Fahrenheit em 
+uma matriz 3 × 3, denominada de A. Construa outra matriz, denominada de B, do 
+mesmo tipo e tamanho de A, onde cada elemento de B deve ser a conversão da 
+temperatura em Fahrenheit para Celsius do elemento correspondente da matriz A. 
+Ao final, o programa deve exibir o conteúdo de A e B.
+Obs.: para conversão, utilize a fórmula: c = (f − 32) / 1,8. */
