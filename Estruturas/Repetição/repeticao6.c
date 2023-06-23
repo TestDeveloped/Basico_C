577f6113158ce4bc44f3f52026ae7b98e2840a28
TestDeveloped <137568401+TestDeveloped@users.noreply.github.com>
2023-06-23 23:00:56 +0000

    Estruturas

---
+++ Estruturas/Repetição/repeticao6.c
@@ -0,0 +1,6 @@
+/*Faça uma função, com retorno, para calcular o valor do volume de uma caixa 
+retangular, utilizando a seguinte fórmula:
+𝑣𝑜𝑙𝑢𝑚𝑒 = 𝑐𝑜𝑚𝑝𝑟𝑖𝑚𝑒𝑛𝑡𝑜 × 𝑙𝑎𝑟𝑔𝑢𝑟𝑎 × 𝑎𝑙𝑡𝑢𝑟𝑎
+Obs.: os valores do comprimento, da largura e da altura devem ser fornecidos pelo 
+usuário e devem ser passados como argumento para a função. A função main deve 
+receber e exibir o resultado do cálculo. */
